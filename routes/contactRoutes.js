8307bbdb7f0207469b49424892d91a4d667e2d09
Keshia Adrin <keshady26.bae@gmail.com>
2025-11-02 23:21:53 +0530

    first commit

---
+++ routes/contactRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+} = require("../controllers/contactController");
+
+const router = express.Router();
+
+router.route("/").get(getContacts).post(createContact);
+
+router.route("/:id").get(getContact);
+
+router.route("/:id").put(updateContact);
+
+router.route("/:id").delete(deleteContact);
+
+module.exports = router;
